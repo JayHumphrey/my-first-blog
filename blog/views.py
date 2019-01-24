bc284c7f46f18fdc87ce87ebcc51b5b09ae1b465
Jay <jayham143@gmail.com>
2019-01-24 11:03:14 +0800

    update

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from django.utils import timezone
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -14,13 +14,13 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-       form = PostForm(request.POST)
-       if form.is_valid():
-           post = form.save(commit=False)
-           post.author = request.user
-           post.published_date = timezone.now()
-           post.save()
-           return redirect('post_detail', pk=post.pk)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
